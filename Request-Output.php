b3205d4b2ac809277e3df6eb476838ee42963cf7
evanzieg <60709447+evanzieg@users.noreply.github.com>
2020-04-25 14:21:52 -0400

    unjankified code

--- Request-Output.php
+++ Request-Output.php
@@ -11,9 +11,7 @@
     <h1>You're here</h1>
     <?php
         $prof_name = $_POST['professor'];
-        $name_space = strpos($prof_name, " ");
-        $prof_last_name = substr($prof_name, $name_space);
-        $prof_file_name = $prof_last_name.".xml";
+        $prof_file_name = $prof_name.".xml";
 
         echo $prof_file_name;
     ?>

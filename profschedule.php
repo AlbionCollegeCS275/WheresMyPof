473ec74887690100c1790113ee85149c7f4deb20
kmi11 <62416506+kmi11@users.noreply.github.com>
2020-04-25 14:46:21 -0400

    Add files via upload

---
+++ profschedule.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+if(isset($_POST['submit'])) {
+echo "<h2>Form Submitted Successfully!! Bellow is the data</h2>";
+      echo "<hr>";
+      echo "</br>Your Name :{$_POST['lname']}";
+      echo "</br>Office Hours :{$_POST['officebuilding']}";
+      echo "<hr>";
+   }
+
+
+
+}
+
+
+
+
+
+ ?>
+</body>
+</html>

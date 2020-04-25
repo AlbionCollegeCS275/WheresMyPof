be3aecc696afcda2a6a786f0972bfbea1b183e7d
evanzieg <60709447+evanzieg@users.noreply.github.com>
2020-04-25 13:48:09 -0400

    Shorten professor's name to just last name to match xml file name format

--- Request-Output.php
+++ Request-Output.php
@@ -10,7 +10,10 @@
 <body>
     <h1>You're here</h1>
     <?php
-        echo $_POST['professor'];
+        $prof_name = $_POST['professor'];
+        $name_space = strpos($prof_name, " ");
+        $prof_last_name = substr($prof_name, $name_space);
+        echo $prof_last_name;
     ?>
 </body>
 
